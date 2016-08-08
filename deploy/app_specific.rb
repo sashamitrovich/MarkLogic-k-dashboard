846e93f09c40f1bee0d44c937e028c30e7876301
Saša Mitrović <sasha.mitrovich@marklogic.com>
2016-08-08 10:02:23 +0200

    Fixed a case when the price csv is empty but response was 200 - should be logged and skipped

--- deploy/app_specific.rb
+++ deploy/app_specific.rb
@@ -56,37 +56,40 @@ def load_stock_price()
 
     declare namespace x= "xdmp:http";
 
-    let $docs:=fn:collection("code")
+    let $docs:= fn:collection("code")
     for $doc in $docs
       let $request:=fn:concat("https://www.quandl.com/api/v3/datasets/FSE/",$doc//Symbol, "_X.csv?api_key=yigbEs6PAybUcxg6Lz_A&amp;start_date=2016-07-25")
       let $response:=xdmp:http-get($request)
       return if ($response//x:code=200) then      
         let $quote:= $response[2]
-        let $prices:=util:parse-price-csv($quote)
-        let $newDoc:=document {
-          element stock {
-            $doc/@*,
-            $doc/stock/*,
-            element type {"stock"},       
-            element source {"Quandl"},
-             for $price at $pos in $prices
-                let $empty:= fn:empty($price/Change/text())
-                let $change:= if($empty) then 0 else $price/Change/text()
-                let $close:=$price/Close/text()
-                let $open:=($close - $change)
-                let $percent:=($change * 100) div $open
-                return
-                if ($pos = 1) then element price-latest { $price/*, element Percent { $percent } }
-                else element price { $price/*, element Percent { $percent } }   
-            }
-        }
-        let $newUri:=fn:concat($newDoc//Symbol)
-        let $diff:= round-half-to-even(($newDoc//Close)[1]-($newDoc//Close)[8],2)
-        let $week-change-percent:=round-half-to-even(($diff * 100) div ($newDoc//Close)[1], 2)
-        let $week-change-percent-element:=element week-change-percent {$week-change-percent}
-        let $week-change:=element week-change { $diff }
-        let $newDoc:=mem:node-insert-after($newDoc/stock/source,$week-change-percent-element)
-        return xdmp:document-insert($newUri, $newDoc,(),("data","stock-price"))
+        let $prices:=util:parse-price-csv($quote)   
+        return if(fn:count($prices)>1) then
+          let $newDoc:=document {
+            element stock {
+              $doc/@*,
+              $doc/stock/*,
+              element type {"stock"},       
+              element source {"Quandl"},
+               for $price at $pos in $prices
+                  let $empty:= fn:empty($price/Change/text())
+                  let $change:= if($empty) then 0 else $price/Change/text()
+                  let $close:=$price/Close/text()
+                  let $open:=($close - $change)
+                  let $percent:=($change * 100) div $open
+                  return
+                  if ($pos = 1) then element price-latest { $price/*, element Percent { $percent } }
+                  else element price { $price/*, element Percent { $percent } }   
+              }
+          }
+          let $newUri:=fn:concat($newDoc//Symbol)
+          let $diff:= round-half-to-even(($newDoc//Close)[1]-($newDoc//Close)[8],2)
+          let $week-change-percent:=round-half-to-even(($diff * 100) div ($newDoc//Close)[1], 2)
+          let $week-change-percent-element:=element week-change-percent {$week-change-percent}
+          let $week-change:=element week-change { $diff }
+          let $newDoc:=mem:node-insert-after($newDoc/stock/source,$week-change-percent-element)
+          return xdmp:document-insert($newUri, $newDoc,(),("data","stock-price"))
+        else 
+          xdmp:log(concat("skipping ", $doc//Symbol))
       else
         xdmp:log(concat("skipping ", $doc//Symbol))
     },
